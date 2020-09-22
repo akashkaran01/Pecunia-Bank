d20840168b2808b878b7e16dad744eaa9dbeec24
Akash Karn <akashkaran01@gmail.com>
2020-09-22 17:05:18 +0530

    Intital commimt

---
+++ src/main/java/com/cg/bank/dao/TransactionDAO.java
@@ -0,0 +1,11 @@
+package com.cg.bank.dao;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+interface TransactionDAO {
+
+	//Account getAccountById(String a);
+	//double getBalance()
+	
+}
